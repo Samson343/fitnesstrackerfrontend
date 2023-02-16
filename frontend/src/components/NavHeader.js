3753d0d73abf7a5b1c44eb96b9ba338365e5352f
Samson343 <113782614+Samson343@users.noreply.github.com>
2023-02-16 11:42:38 -0700

    Created the NavHeader

--- frontend/src/components/NavHeader.js
+++ frontend/src/components/NavHeader.js
@@ -6,6 +6,25 @@ import styles from './NavHeader.module.css'
 export const NavHeader = () => {
     return (
         <header className= {styles.NavHeader}>
+          <span className = {styles.navHeading}>Fitness track.er</span>
+          <span className={styles.menuContainers}>
+            <Link to="/activities" className={styles.link} title='View available exercises in the database, or add a new one'>
+              <h4>Activities</h4>
+            </Link>
+            <Link className={styles.link} to='/routines' title='Create a new routine or select an existing one'>
+              <h4>Routines</h4>
+            </Link>
+            <Link className={styles.link} to="/profile" title='Profile'>
+              <h4>Profile</h4>
+            </Link>
+          </span>
+          {/* <Button variant="outlined" color="error">
+            Register/Login
+          </Button>  */}
+          {/* styling mui components is harder than it's worth lol */}
+          <Link to = "/login">
+            <button className= {styles.Button}>Register/Login</button>
+          </Link>
             
         </header>
     )
