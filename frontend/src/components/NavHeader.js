30f381c71cd379571862d2b113890d63b32099c3
Samson343 <113782614+Samson343@users.noreply.github.com>
2023-02-17 08:08:08 -0700

    Imported Link
    accidentally left it out when I pasted this in from my branch

--- frontend/src/components/NavHeader.js
+++ frontend/src/components/NavHeader.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styles from './NavHeader.module.css'
+import { Link } from 'react-router-dom'
 
 //A header with icons and/or menus to navigate to different components
 
