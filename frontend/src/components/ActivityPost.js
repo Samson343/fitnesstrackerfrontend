e260159657bda2b24537eaaa3cc5e087df5195fb
carowoline <caro98.jpg@gmail.com>
2023-02-17 21:22:10 -0500

    finally fixed so that you can add new activities

--- frontend/src/components/ActivityPost.js
+++ frontend/src/components/ActivityPost.js
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { callApi } from "../api/apiCalls";
 import styles from './ActivityPost.module.css';
 import { TextField, Button } from "@mui/material";
 
-const ActivityPost = async ({ token }) => {
+const ActivityPost = ({ token }) => {
     const [newActivity, setNewActivity] = useState({ name: "", description: "" });
 
-    const submitHandler = async (event) => {
-        event.preventDefault();
-    };
+    const createActivity = useCallback(async () => {
+        try {
+            const response = await callApi({
+                url: "activities",
+                method: "POST",
+                token,
+                body: newActivity
+            });
 
-    try {
-        const response = await callApi({
-            url: "activities",
-            method: "POST",
-            token,
-            body: newActivity
-        });
-
-        if (response) {
-            setNewActivity({ name: "", description: "" });
+            if (response) {
+                setNewActivity({ name: "", description: "" });
+            }
         }
-    }
-    catch (error) {
-        console.error(error);
-    };
+        catch (error) {
+            console.error(error);
+        }
+    }, [newActivity, token]);
+
+    useEffect(() => {
+        createActivity();
+    }, [newActivity, token, createActivity]);
 
     const changeName = (event) => {
         setNewActivity({ ...newActivity, name: event.target.value });
@@ -34,30 +36,37 @@ const ActivityPost = async ({ token }) => {
         setNewActivity({ ...newActivity, description: event.target.value });
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        createActivity();
+    };
+
     return (
-        <form onSubmit={submitHandler} className={styles.createForm}>
-            <TextField
-                size="small"
-                className={styles.formInputs}
-                id="outlined-basic"
-                label="Name"
-                variant="outlined"
-                value={newActivity.name}
-                onChange={changeName}
-            />
-            <TextField
-                size="small"
-                className={styles.formInputs}
-                id="outlined-basic"
-                label="Description"
-                variant="outlined"
-                value={newActivity.description}
-                onChange={changeDescription}
-            />
-            <Button size="small" className={styles.submitButton} type="submit">
-                New Activity
-            </Button>
-        </form>
+        <div>
+            <form onSubmit={submitHandler} className={styles.createForm}>
+                <TextField
+                    size="small"
+                    className={styles.formInputs}
+                    id="outlined-basic"
+                    label="Name"
+                    variant="outlined"
+                    value={newActivity.name}
+                    onChange={changeName}
+                />
+                <TextField
+                    size="small"
+                    className={styles.formInputs}
+                    id="outlined-basic"
+                    label="Description"
+                    variant="outlined"
+                    value={newActivity.description}
+                    onChange={changeDescription}
+                />
+                <Button size="small" className={styles.submitButton} type="submit">
+                    New Activity
+                </Button>
+            </form>
+        </div>
     )
 }
 
